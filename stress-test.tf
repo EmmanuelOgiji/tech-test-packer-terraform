1c92241adf853777d3455d2dee32efdf155661c8
EmmanuelOgiji <epiusogiji@yahoo.com>
2021-01-14 17:01:28 +0000

    Task 3: Stress test - Up stress timeout to 600 - Readd vpc endpoints for ssm

--- stress-test.tf
+++ stress-test.tf
@@ -60,7 +60,7 @@ resource "aws_ssm_document" "trigger_asg_stress" {
          "name": "RunStress",
          "inputs": {
             "runCommand": [
-               "sudo stress -c 50 -v --timeout 60s"
+               "sudo stress -c 50 -v --timeout 300s"
             ]
          }
       }
--- vpc.tf
+++ vpc.tf
@@ -69,3 +69,49 @@ data "aws_vpc_endpoint_service" "ssm" {
   service      = "ssm"
   service_type = "Interface"
 }
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = data.aws_vpc_endpoint_service.ssm.service_name
+  vpc_endpoint_type = "Interface"
+  subnet_ids = aws_subnet.main.*.id
+  security_group_ids = [
+    aws_security_group.instance_sg.id,
+  ]
+
+  private_dns_enabled = true
+}
+
+data "aws_vpc_endpoint_service" "ssmmessages" {
+  service      = "ssmmessages"
+  service_type = "Interface"
+}
+
+resource "aws_vpc_endpoint" "ssmmessages" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = data.aws_vpc_endpoint_service.ssmmessages.service_name
+  vpc_endpoint_type = "Interface"
+  subnet_ids = aws_subnet.main.*.id
+  security_group_ids = [
+    aws_security_group.instance_sg.id,
+  ]
+
+  private_dns_enabled = true
+}
+
+data "aws_vpc_endpoint_service" "ec2messages" {
+  service      = "ec2messages"
+  service_type = "Interface"
+}
+
+resource "aws_vpc_endpoint" "ec2messages" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = data.aws_vpc_endpoint_service.ec2messages.service_name
+  vpc_endpoint_type = "Interface"
+  subnet_ids = aws_subnet.main.*.id
+  security_group_ids = [
+    aws_security_group.instance_sg.id,
+  ]
+
+  private_dns_enabled = true
+}
